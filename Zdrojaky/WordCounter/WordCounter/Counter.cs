5abc5c54a76475c1851ab7fefe1faa857625d64f
et <evkat@users.noreply.github.com>
2019-01-25 21:22:49 +0100

    Add files via upload

---
+++ Zdrojaky/WordCounter/WordCounter/Counter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WordCounter
+{
+    /// <summary>
+    /// Trieda na reprezentaciu poctu slov v suboer
+    /// </summary>
+    public class Counter
+    {
+        /// <summary>
+        /// konstruktor triedy
+        /// </summary>
+        /// <param name="fileContent">obsah suboru</param>
+        public Counter(string fileContent)
+        {
+            // interpunkcia
+            var punctuation = fileContent.Where(Char.IsPunctuation).Distinct().ToArray();
+            // rozdelim obsah suboru a orezem interpunkciu
+            var words = fileContent.ToLower().Split().Select(x => x.Trim(punctuation));
+            // vyberiem unikatne rozparsovane stringy dlhsie ako 0 znakov
+            var uniqueWords = words.Where(x => x.Length > 0).Distinct().ToArray();
+            // naplnim dictionary
+            uniqueWords.ToList().ForEach(x => wordsAsIs.Add(x, words.Where(s => s == x).Count()));
+
+        }
+
+        /// <summary>
+        /// pocty slov v poradi v akom boli nacitane zo suboru
+        /// </summary>
+        private Dictionary<string, int> wordsAsIs = new Dictionary<string, int>();
+        /// <summary>
+        /// pocty slov zoradene podla abecedy
+        /// </summary>
+        private Dictionary<string, int> OrderedAlphabet => wordsAsIs.OrderBy(x => x.Key).ToDictionary(item => item.Key, item => item.Value);
+        /// <summary>
+        /// pocty slov zoradene podla vyskytu
+        /// </summary>
+        private Dictionary<string, int> OrderedByCount => wordsAsIs.OrderBy(x => x.Key).OrderByDescending(x => x.Value).ToDictionary(item => item.Key, item => item.Value);
+
+        /// <summary>
+        /// prepisanie metody to string - umozni ziskat vystup v dobe vystupneho retazca
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string outputString =
+
+            $"*** Slova v poradi v sobore ***\n{GenerateOutputString(wordsAsIs)}" +
+            $"\n\n*** Slova podla abacedy ***\n{GenerateOutputString(OrderedAlphabet)}" +
+            $"\n\n*** Slova podla vyskytu ***\n{GenerateOutputString(OrderedByCount)}";
+
+            return outputString;
+        }
+
+        /// <summary>
+        /// metoda na generovanie retazca hodnot z dictionary
+        /// </summary>
+        /// <param name="dict"></param>
+        /// <returns></returns>
+        private string GenerateOutputString(Dictionary<string, int> dict)
+        {
+            string outputString = string.Empty;
+            dict.ToList().ForEach
+                (
+                x =>
+                {
+                    outputString += $"\nSlovo: {x.Key}, pocet {x.Value}";
+                }
+                );
+            return outputString;
+        }
+    }
+}
---
+++ Zdrojaky/WordCounter/WordCounter/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace WordCounter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // ziaden parameter
+            if (args.Count() == 0)
+            {
+                Console.WriteLine("Chybajuca cesta k soboru. Program bude ukonceny.");
+                Console.ReadKey();
+                return;
+            }
+            if (args.Count()>1)
+            {
+                Console.WriteLine("Zadanych privela vstupnych parametrov. Pocitat slova sa budu len pre prvy parameter.");
+            }
+
+            string filePath = args[0];
+            string content = string.Empty;
+            try
+            {
+                content = File.ReadAllText(filePath);
+            }
+            catch
+            {
+                Console.WriteLine("Chyba pri nacitani suboru. Program bude ukonceny");
+                Console.ReadKey();
+                return;
+            }
+
+            if (content == string.Empty)
+            {
+                Console.WriteLine("Prazdny subor. Program bude ukonceny");
+                Console.Read();
+                return;
+            }
+
+            Counter counter = new Counter(content);
+            if (counter != null)
+            {
+                Console.WriteLine("Subor nacitany.");
+                string outputFilePath = string.Empty;
+                var result = WriteOutput(counter, filePath, out outputFilePath);
+                if (result)
+                    Console.WriteLine($"Vysledky su v subore {outputFilePath}.");
+                else
+                    Console.WriteLine("Chyba pri zapise vysledkov.");
+                Console.ReadKey();
+            }
+        }
+
+        /// <summary>
+        /// metoda pre zapis vysledkov do suboru
+        /// </summary>
+        /// <param name="counter">pocitac slov</param>
+        /// <param name="inputFilePath">cesta k vstupnemu suboru</param>
+        /// <param name="outputFilePath">cestka k vystupnemu suboru</param>
+        /// <returns>priznak, ci bol zapis do suboru uspesny</returns>
+        private static bool WriteOutput(Counter counter, string inputFilePath, out string outputFilePath)
+        {
+            var directory = Path.GetDirectoryName(inputFilePath);
+            var fileName = Path.GetFileNameWithoutExtension(inputFilePath);
+            outputFilePath = $"{directory}\\{fileName}_output.txt";
+            if (File.Exists(outputFilePath))
+                Console.WriteLine("Existujuci subor s vysledkami bude prepisany.");
+            string outputString = counter.ToString();
+            try
+            {
+                File.WriteAllText(outputFilePath, outputString);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
